8c967847a89215f9aaf79af84b121b8a0b887647
ウェンベル <wenvelk@gmail.com>
2025-03-13 08:38:20 -0300

    Add files via upload

---
+++ salario/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Quantos funcionários deseja cadastrar? ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        double[] salarios = new double[quantidade];
+
+        for (int i = 0; i < quantidade; i++)
+        {
+            Console.Write($"Digite o salário do seu funcionário {i + 1}: R$ ");
+            salarios[i] = double.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nSalários reajustados:");
+
+        for (int i = 0; i < quantidade; i++)
+        {
+            double aumento = salarios[i] <= 500 ? 0.10 : 0.08;
+            double novoSalario = salarios[i] * (1 + aumento);
+            Console.WriteLine($"Funcionário {i + 1}: R$ {novoSalario:F2}");
+        }
+    }
+}
